e812d9c4197a05f2cf83c55e56f7cd7b97a0a509
Brian Wesnofske <bwesnofske@gmail.com>
2017-10-21 10:02:03 -0400

    Inserted Video with video controls Corrected Color selection of background for winner Added stalemate handling

--- app/src/main/java/com/example/android/connect3/MainActivity.java
+++ app/src/main/java/com/example/android/connect3/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.android.connect3;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -16,15 +18,49 @@ public class MainActivity extends AppCompatActivity {
 
     int duration = Toast.LENGTH_SHORT;
 
+    // used to track a stalemate
+    int staleMate = 0;
+
 
     // Turn Tracker - red always goes first
     private String turn = "red";
 
+    // Timer used to give enough time for animations to finish before pop up screen appears
+    CountDownTimer delay = new CountDownTimer(2500, 1000) {
+        @Override
+        public void onTick(long millisUntilFinished) {
+
+        }
+
+        @Override
+        public void onFinish() {
+            Intent intent = new Intent(MainActivity.this,Pop.class);
+            intent.putExtra("turn", turn);        //startActivity(new Intent(MainActivity.this,Pop.class));
+            startActivity(intent);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = (Button) findViewById(R.id.playAgainButton2);
+        button.setTranslationY(-1000f);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View v) {
+                Intent i = getBaseContext().getPackageManager()
+                        .getLaunchIntentForPackage(getBaseContext().getPackageName());
+                i.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
+                startActivity(i);
+            }
+
+            ;
+
+
+        });
 
         }
 
@@ -44,25 +80,24 @@ public class MainActivity extends AppCompatActivity {
         } else {
 
                 if (turn == "red") {
-
                     //set correct index in array to true
+                    staleMate++;
                     redArray[clickedImage] = true;
 
                     token.setTranslationY(-1000f);
                     token.setImageResource(R.drawable.red);
                     token.animate().translationYBy(1000f).setDuration(2000);
-                    turn = "yellow";
                     checkArray(redArray);
 
-                } else {
 
+                } else {
+                    staleMate++;
                     //set correct index in array to true
                     yellowArray[clickedImage] = true;
 
                     token.setTranslationY(-1000f);
                     token.setImageResource(R.drawable.yellow);
                     token.animate().translationYBy(1000f).setDuration(2000);
-                    turn = "red";
                     checkArray(yellowArray);
 
                 }
@@ -93,36 +128,43 @@ public class MainActivity extends AppCompatActivity {
             winner();
         } else if ((array[2] && array[4] && array[6]) == true) {
             winner();
+        } else if (this.staleMate == 9) {
+            staleMate();
         } else {
             nextPlayer();
         }
     }
 
+
+    public void staleMate() {
+        Button playAgain2 = (Button) findViewById(R.id.playAgainButton2);
+        playAgain2.animate().translationY(500f);
+        Toast next = Toast.makeText(getApplicationContext(), "Stalemate! Play Again! ", Toast.LENGTH_LONG);
+    }
+
     public void winner() {
-        Toast winner = Toast.makeText(getApplicationContext(), "You're the Winner", Toast.LENGTH_LONG);
-        winner.show();
-        startActivity(new Intent(MainActivity.this,Pop.class));
-
-
+        delay.start();
     }
 
     public void nextPlayer() {
-        Toast next = Toast.makeText(getApplicationContext(), "Next Player", Toast.LENGTH_LONG);
+        if(turn == "red"){
+            turn = "yellow";
+        } else {
+            turn = "red";
+        }
+        Toast next = Toast.makeText(getApplicationContext(), "Next Player", Toast.LENGTH_SHORT);
         next.show();
 
     }
 
     public void spaceUsed() {
-        Toast next = Toast.makeText(getApplicationContext(), "Space Already Chosen, Choose Again", Toast.LENGTH_LONG);
+        Toast next = Toast.makeText(getApplicationContext(), "Space Already Chosen, Choose Again", Toast.LENGTH_SHORT);
         next.show();
 
     }
 
+
 }
 
 
-
-    //On click choice
-
-    //We could have both images in place for each spot and have one appear when necc.  We could try to change source of picture on the fly
 
--- app/src/main/java/com/example/android/connect3/Pop.java
+++ app/src/main/java/com/example/android/connect3/Pop.java
@@ -2,10 +2,16 @@ package com.example.android.connect3;
 
 import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.Button;
+import android.widget.MediaController;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+import android.widget.VideoView;
 
 /**
  * Created by Brian Wesnofske on 10/15/2017.
@@ -19,9 +25,44 @@ class Pop extends Activity {
 
         setContentView(R.layout.popwindow);
 
-        Button button = (Button) findViewById(R.id.button);
+        // Video view to play victory video
+        VideoView videoView = (VideoView) findViewById(R.id.videoView);
 
-        button.setOnClickListener(new View.OnClickListener() {
+        videoView.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.confetti);
+
+        // Controller to manipulate videoo
+        MediaController mediaController = new MediaController(this);
+
+        mediaController.setAnchorView(videoView);
+
+        videoView.setMediaController(mediaController);
+
+        videoView.start();
+
+
+        Button playAgain = (Button) findViewById(R.id.button);
+
+        TextView textView = (TextView) findViewById(R.id.textViewPop);
+
+        RelativeLayout popLayout2 = (RelativeLayout) findViewById(R.id.popLayout);
+
+        // String used to check whose turn it was on the winning move.
+        String winner = getIntent().getStringExtra("turn");
+
+
+        Toast next = Toast.makeText(getApplicationContext(), winner, Toast.LENGTH_LONG);
+        next.show();
+
+        if (winner.equalsIgnoreCase("red")) {
+            textView.setText("Congrats " + winner.toUpperCase() + " Player Won!");
+            popLayout2.setBackgroundColor(Color.parseColor("#ffcc0000"));
+        } else {
+            textView.setText("Congrats " + winner.toUpperCase() + " Player Won!");
+            popLayout2.setBackgroundColor(Color.parseColor("#ffffbb33"));
+        }
+
+
+        playAgain.setOnClickListener(new View.OnClickListener() {
 
             @Override
             public void onClick(View v) {
